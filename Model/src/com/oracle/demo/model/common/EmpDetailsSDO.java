2690242915616dceb948b07650322d71bcc6533f
Oscar I. Hernandez V <o@malericarum.mx>
2013-03-05 13:25:19 -0600

    Added almos final version

---
+++ Model/src/com/oracle/demo/model/common/EmpDetailsSDO.java
@@ -0,0 +1,47 @@
+package com.oracle.demo.model.common;
+
+public interface EmpDetailsSDO {
+
+   public java.lang.Integer getEmployeeId();
+
+   public void setEmployeeId(java.lang.Integer value);
+
+   public java.lang.String getFirstName();
+
+   public void setFirstName(java.lang.String value);
+
+   public java.lang.String getLastName();
+
+   public void setLastName(java.lang.String value);
+
+   public java.lang.String getEmail();
+
+   public void setEmail(java.lang.String value);
+
+   public java.math.BigDecimal getSalary();
+
+   public void setSalary(java.math.BigDecimal value);
+
+   public java.sql.Timestamp getHireDate();
+
+   public void setHireDate(java.sql.Timestamp value);
+
+   public java.lang.String getJobId();
+
+   public void setJobId(java.lang.String value);
+
+   public java.lang.Integer getDepartmentId();
+
+   public void setDepartmentId(java.lang.Integer value);
+
+   public java.lang.String getDepartmentName();
+
+   public void setDepartmentName(java.lang.String value);
+
+   public java.math.BigDecimal getAnnualSalary();
+
+   public void setAnnualSalary(java.math.BigDecimal value);
+
+
+}
+
